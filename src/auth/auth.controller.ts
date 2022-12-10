da5d1d22c27628944b5dbcd8c06d7e690b7e0b56
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2022-12-10 19:59:56 -0300

    added functions to return the token on login function

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Controller('/api/auth')
 export class AuthController {
 
+    constructor (private readonly authService: AuthService){}
+
     @UseGuards(AuthGuard('local'))
     @Post('login')
     async login(@Req() req:any){
-        return req.user;
+        return await this.authService.login(req.user);
     }
 
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { compareSync } from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly userService: UsersService){}
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
+        ){}
+
+    async login(user){
+        const payload = { sub: user.id, email: user.email }
+
+        return{
+            token: this.jwtService.sign(payload)
+        }
+    }
 
     async validateUser(email: string, password: string){
        
@@ -16,7 +28,7 @@ export class AuthService {
         }catch (error){
             return null;
         }
-                
+
         const isPasswordValid = compareSync(password, user.password)
         if(isPasswordValid){
             return user;
