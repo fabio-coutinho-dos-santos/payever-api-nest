1a815219a871bb8ec8effce726d6017f689d7d0b
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-05-28 19:00:48 -0300

    added supertest and updated way to make the connection with database based it on NODE_ENV variable

--- src/users/tests/integration/users.controller.integration.spec.ts
+++ src/users/tests/integration/users.controller.integration.spec.ts
@@ -2,6 +2,7 @@ import { Test } from "@nestjs/testing"
 import { Connection } from "mongoose"
 import { AppModule } from "src/app.module"
 import { DatabaseService } from "src/database/database.service";
+import { userStub } from "../stubs/user.stub";
 
 describe('UsersController' , () => {
 
@@ -14,7 +15,11 @@ describe('UsersController' , () => {
 
         const app = moduleRef.createNestApplication();
         await app.init();
-        
+
         dbConnection = moduleRef.get<DatabaseService>(DatabaseService).getDbHandle();
+
+        describe('getUsers', async () => {
+            await dbConnection.collection('users').insertOne(userStub());
+        })
     })
 })
