9a626112689d7c126989042832afc3529ffb0072
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2023-05-28 18:29:33 -0300

    added files to allow get the database connection in all parts of code

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_DB_NAME_TEST}:${process.env.MONGO_DB_PASSWORD_TEST}@${process.env.MONGO_DB_CLUSTER_TEST}.iboevg3.mongodb.net/nestjs-invite?retryWrites=true&w=majority`),
     UsersModule,
     AuthModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
